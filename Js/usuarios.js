a04cdfe0a290ad0117a61b3045cb94dccf3a3a69
Fede1979 <fedesanchez@gmail.com>
2024-12-20 18:03:24 -0300

    modificando usuarios y linkeando a index

--- Js/usuarios.js
+++ Js/usuarios.js
@@ -1,4 +1,4 @@
-// LOGIN y LOGOUT USUARIOS
+// SIGNIN USUARIOS
 
 const registroUsuario = []
 
@@ -12,47 +12,15 @@ class DatosUsuario {
 }
 
 let usuarioNombre = prompt("Ingrese su nombre")
+
 let usuarioApellido = prompt("Ingrese su apellido")
+
 let usuarioCorreo = prompt("Ingrese su correo electrónico")
 
-let nuevoUsuario = new DatosUsuario (usuarioNombre, usuarioApellido, usuarioCorreo)
-registroUsuario.push = nuevoUsuario
-}
-
-const usuarios = []
-const contrasenas = []
-
-LogInUsuario = () => {
-    let altaUsuario = prompt("Por favor ingrese su correo electrónico")
-    if (altaUsuario == ""){
-        alert("Debe ingresar su correo electrónico")
-    }else{
-        alert("Usuario ingresado")
-    }
-
-    let altaContrasena = parseInt(prompt("Por favor ingrese su contraseña numérica"))
-    if (altaContrasena == ""){
-        alert("Debe ingresar una contraseña")
-    }else{
-        alert("Contraseña ingresada")
-    }
-
-    let ingresoUsuario = altaUsuario.push(usuarios)
-
-    if(ingresoUsuario !== this.correo){
-        alert("Usuario no registrado")
-    }else{
-        alert("Bienvenido " + this.nombre)
-    }
-    let ingresoContrasena = altaContrasena.push(contrasenas)
-
-    if(ingresoContrasena !== isNaN){
-        alert("Valor ingresado no válido")
-    }else{
-        alert("Contraseña aceptada")
-    }
+let usuarioUsuario = new DatosUsuario (usuarioNombre, usuarioApellido, usuarioCorreo)
+registroUsuario.push(usuarioUsuario)
 }
 
 SignInUsuario()
 
-LogInUsuario()
+console.log(registroUsuario)
