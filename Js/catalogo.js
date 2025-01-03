30a413ccc38e499dac5bd18c1c6c2f1787b20c9b
Fede1979 <fedesanchez@gmail.com>
2025-01-03 17:17:13 -0300

    agregue metodo forEach en catalogo

--- Js/catalogo.js
+++ Js/catalogo.js
@@ -7,14 +7,20 @@ const cotizacion = 1100
 
 class Celular {
     static id = 0
-    constructor(marca, modelo, cantidad, precio, subtotal){
+    constructor(marca, modelo, cantidad, precio){
         this.id = ++Celular.id,
         this.marca = marca,
         this.modelo = modelo,
         this.cantidad = cantidad,
-        this.precio = precio,
-        this.subtotal = subtotal
+        this.precio = precio
     }       
+    calcularSubtotal() {
+        return this.cantidad * (this.precio * cotizacion);
+    }
+
+    calcularPrecioConIva() {
+        return this.calcularSubtotal() * 1.21;
+    }
 }
 
 const productos = []
@@ -24,35 +30,50 @@ const agregarCatalogo = () => {
     let agregarModelo = prompt("Por favor, ingrese el modelo")
     let agregarCantidad = parseInt(prompt("Por favor, ingrese la cantidad"))
     let agregarPrecioDolares = parseInt(prompt("Por favor, ingrese el precio en dolares"))
-    let agregarSubtotal = agregarCantidad*(agregarPrecioDolares*cotizacion)
 
-    const agregarCelular = new Celular(agregarMarca, agregarModelo, agregarCantidad, agregarPrecioDolares, agregarSubtotal)
-    productos.push(agregarCelular)
+    if (isNaN(agregarCantidad) || isNaN(agregarPrecioDolares) || agregarCantidad <= 0 || agregarPrecioDolares <= 0) {
+        alert("Los valores ingresados no son válidos. Intente de nuevo.");
+        return;
+    }else {
+        const agregarCelular = new Celular(agregarMarca, agregarModelo, agregarCantidad, agregarPrecioDolares)
+        productos.push(agregarCelular)
+        alert(`Producto agregado: ${agregarMarca} ${agregarModelo}`)
+    }    
 }
 
 const verCatalogo = () => {
-    if(productos == 0){
-        console.log("No hay productos agragados")
-    }else{
-        for(const producto of productos)
-        console.log(`Marca: ${producto.marca}\nModelo: ${producto.modelo}\nCantidad: ${producto.cantidad}\nPrecio en dólares: ${producto.precio}\nSubtotal en Pesos: $ ${producto.subtotal}`)
+    if (productos.length === 0) {
+        alert("No hay productos agregados al catálogo.");
+        return
+    }else {
+        productos.forEach(producto => {
+        alert(`ID: ${producto.id}\n` + `Marca: ${producto.marca}\n` + `Modelo: ${producto.modelo}\n` + `Cantidad: ${producto.cantidad}\n` + `Precio en dólares: ${producto.precio}\n` + `Subtotal en pesos: ${producto.calcularSubtotal()} \n` + `Precio con IVA: ${producto.calcularPrecioConIva()}`)
+        })
     }
 }
 
-let menu = parseInt(prompt("Bienvenido al menú de administrador: \n" + "Por favor elija una opción\n" + "1.- Agregar un celular al catálogo\n" + "2.- Ver catálogo\n" + "3.- Salir del menú"))
+const menuPrincipal = () => {
+    let opcion;
+  
 
-while (menu !== 3){
-    switch(menu) {
-        case 1:
-            agregarCatalogo()
-            break
-        case 2:
-            verCatalogo()
-            break
-        default: 
-            console.log("Opción no válida")       
-    }
-    menu = parseInt(prompt("Bienvenido al menú de administrador: \n" + "Por favor elija una opción\n" + "1.- Agregar un celular al catálogo\n" + "2.- Ver catálogo\n" + "3.- Salir del menú"))
-}
+    do {
+        opcion = parseInt(prompt("Bienvenido al menú de administrador:\n" + "1. Agregar un celular al catálogo\n" + "2. Ver catálogo\n" + "3. Salir"))
 
+        switch (opcion) {
+            case 1:
+                agregarCatalogo()
+                break
+            case 2:
+                verCatalogo()
+                break
+            case 3:
+                alert("Gracias por usar el sistema.")
+                break
+            default:
+                alert("Opción no válida. Intente de nuevo.")
+        }
+    } while (opcion !== 3)
+}        
+
+menuPrincipal()
 console.log(productos)
