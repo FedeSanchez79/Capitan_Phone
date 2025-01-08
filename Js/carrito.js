848d14d18aa8038916981932633a7544c052c85d
Federico Sanchez <fedesanchez@gmail.com>
2025-01-08 11:35:52 -0300

    modifique ruta js en index, estilos boton y commentario carrito

--- Js/carrito.js
+++ Js/carrito.js
@@ -1,5 +1,7 @@
 productos = []; //ingresar los productos del catalogo a este array
 
-let total = productos.reduce ((acumulador, producto) => acumulador + producto.precio, 0) //sujeto a modificacion de acuerdo a como una al cataloo con el carrito
+
+//variable del total del carrito de compras
+let total = productos.reduce ((acumulador, producto) => acumulador + producto.precio, 0) //sujeto a modificacion de acuerdo a como una al catalogo con el carrito
 
 console.log(total)
