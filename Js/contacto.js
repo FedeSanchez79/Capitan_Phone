8fa5f943ba5cf74c54bde517264055d2b273c873
Fede1979 <fedesanchez@gmail.com>
2025-01-11 10:15:21 -0300

    creacion de contacto.js y uso de modificacion de nodos

---
+++ Js/contacto.js
@@ -0,0 +1,4 @@
+let nuevoTitulo = document.getElementById(`info`);
+let tituloContacto = document.createElement(`p`);
+tituloContacto.innerHTML = `<p>¡Estamos a tu disposición!</p>`;
+nuevoTitulo.appendChild(tituloContacto)
