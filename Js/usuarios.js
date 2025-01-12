0b8d467595db7d6426203a995be1c1a8f20e0193
Fede1979 <fedesanchez@gmail.com>
2025-01-12 10:03:03 -0300

    addEventListener para registro, necesita integrarse al Form

--- Js/usuarios.js
+++ Js/usuarios.js
@@ -11,41 +11,45 @@
 
 // SIGNIN USUARIOS
 
-const registroUsuario = [];
+let ingresar = document.getElementsByClassName(`botonLog`)[0];
+ingresar.addEventListener(`click`, function(){
+    const registroUsuario = [];
 
-SignInUsuario = () => {
-class DatosUsuario {
-    constructor (nombre, apellido, correo){
-        this.nombre = nombre,
-        this.apellido = apellido,
-        this.correo = correo
+    SignInUsuario = () => {
+    class DatosUsuario {
+        constructor (nombre, apellido, correo){
+            this.nombre = nombre,
+            this.apellido = apellido,
+            this.correo = correo
+        };
+        esAdmin(){
+            return this.nombre === "Federico" && this.apellido === "Sanchez" && this.correo === "fedesanchez@gmail.com";
+        };
     };
-    esAdmin(){
-        return this.nombre === "Federico" && this.apellido === "Sanchez" && this.correo === "fedesanchez@gmail.com";
-    };
-};
-
-do{
-    let usuarioNombre = prompt("Ingrese su nombre");
-    let usuarioApellido = prompt("Ingrese su apellido");
-    let usuarioCorreo = prompt("Ingrese su correo electrónico");
-    let usuarioUsuario = new DatosUsuario (usuarioNombre, usuarioApellido, usuarioCorreo);
-    registroUsuario.push(usuarioUsuario);
-    alert(`Bienvenido ${usuarioNombre} ${usuarioApellido}, usted esta registrado correctamente`);
-
-    }while(registroUsuario.length === 0){
-        alert(`Registro finalizado`);
-}
-
-    if(esAdmin()){
-        alert(`Bienvenido ${usuarioNombre} ${usuarioApellido}, usted es Administrador`);
-    }else if(usuarioNombre === "" || usuarioApellido === "" || usuarioCorreo === ""){
-        alert(`Debe completar todos los campos`);
-    }else {
+    
+    do{
+        let usuarioNombre = prompt("Ingrese su nombre");
+        let usuarioApellido = prompt("Ingrese su apellido");
+        let usuarioCorreo = prompt("Ingrese su correo electrónico");
+        let usuarioUsuario = new DatosUsuario (usuarioNombre, usuarioApellido, usuarioCorreo);
+        registroUsuario.push(usuarioUsuario);
         alert(`Bienvenido ${usuarioNombre} ${usuarioApellido}, usted esta registrado correctamente`);
+    
+        }while(registroUsuario.length === 0){
+            alert(`Registro finalizado`);
+    }
+    
+        if(esAdmin()){
+            alert(`Bienvenido ${usuarioNombre} ${usuarioApellido}, usted es Administrador`);
+        }else if(usuarioNombre === "" || usuarioApellido === "" || usuarioCorreo === ""){
+            alert(`Debe completar todos los campos`);
+        }else {
+            alert(`Bienvenido ${usuarioNombre} ${usuarioApellido}, usted esta registrado correctamente`);
+        };
     };
-};
+    
+    SignInUsuario()
+    
+    console.log(registroUsuario)
 
-SignInUsuario()
-
-console.log(registroUsuario)
+});
