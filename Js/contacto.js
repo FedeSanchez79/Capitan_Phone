149e8dbd510bc33884db7fb6ff1597ece16ad897
Fede1979 <fedesanchez@gmail.com>
2025-01-12 09:26:02 -0300

    uso de className en contacto y modificacion de class por id en style

--- Js/contacto.js
+++ Js/contacto.js
@@ -1,4 +1,5 @@
 let nuevoTitulo = document.getElementById(`info`);
 let tituloContacto = document.createElement(`p`);
-tituloContacto.innerHTML = `<p>¡Estamos a tu disposición!</p>`;
-nuevoTitulo.appendChild(tituloContacto)
+tituloContacto.innerText = `¡Estamos a tu disposición!`;
+nuevoTitulo.appendChild(tituloContacto);
+tituloContacto.className = `estamos`;
