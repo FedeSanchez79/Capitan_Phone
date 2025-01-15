cccc1a8619aab6bf69a2d6a07e08af47dfd81ac7
Federico Sanchez <fedesanchez@gmail.com>
2025-01-15 13:01:41 -0300

    catalogo.js con metodo MAP

--- Js/catalogo.js
+++ Js/catalogo.js
@@ -13,15 +13,16 @@
 // -NADA de JS en el html
 
 
+
 const items = document.getElementById("items");
 const menuCarga = document.createElement("div");
-menuCarga.innerHTML = `<p>Marca</p>
-                       <input type="text" id="marca" placeholder="Ingrese la marca">
-                        <p>Modelo</p>
+menuCarga.innerHTML = ` <form id="menuCargaCatalogo" action="" method="post">
+                        <p>Nuevo Item</p>
+                        <input type="text" id="marca" placeholder="Ingrese la marca">
                         <input type="text" id="modelo" placeholder="Ingrese el modelo">
-                        <p>Precio</p>
                         <input type="number" id="precio" placeholder="Ingrese el precio">
-                        <button id="agregar">Agregar</button>`;
+                        <button id="agregar">Agregar</button>
+                        </form> `;                        
 items.appendChild(menuCarga);
 
 const productos = []
@@ -33,38 +34,37 @@ class datoProducto {
         this.precio = precio;
     };
 };
-
-let productoNuevo = document.getElementById("contenedorProductos")
-
-function agregarProducto(agregarCelular) {
-    agregarCelular.forEach((celular) => {
-        const card = document.createElement("div")
-        celular.innerHTML = `<h3>${celular.marca}</h3>
-                             <p>${celular.modelo}</p>
-                             <p>${celular.precio}</p>`
-        
-        contenedorProductos.appendChild(card)                     
-    })
+  
+const catalogo = document.getElementById('contenedorProductos');  
+catalogo.innerHTML = `<h3>Marca: 
+                        ${productos.map(producto => `${producto.marca}`)}
+                        </h3>
+                        <p>Modelo: 
+                        ${productos.map(producto => `${producto.modelo}`)}
+                        </p>
+                        <p>Precio: $ 
+                        ${productos.map(producto => `${producto.precio}`)}
+                        </p>`
+catalogo.appendChild(card)
 
     const nuevoProducto = new datoProducto(marca, modelo, precio);
     productos.push(nuevoProducto);
+    console.log(productos)
 
-    agregarItem()
-}
-agregarProducto(productos)
-console.log(productos)
-    
-function agregarItem () {
-     addButton = document.querySelectorAll("#agregar")
-        addButton.forEach(button => {
-            button.onclick = (e) => {
-                const idItem = e.currentTarget.id
-                const itemElegido = productos.find(producto => producto.id == idItem)
-                productos.push(itemElegido)
-                console.log(productos)
-    
-                localStorage.setItem("productos", JSON.stringify(productos))
-            }
-        })
-    }
 
+// agregarProducto(productos)
+// console.log(productos)
+    
+// function agregarItem () {
+//      addButton = document.querySelectorAll("#agregar")
+//         addButton.forEach(button => {
+//             button.onclick = (e) => {
+//                 const idItem = e.currentTarget.id
+//                 const itemElegido = productos.find(producto => producto.id == idItem)
+//                 productos.push(itemElegido)
+//                 console.log(productos)
+    
+//                 localStorage.setItem("productos", JSON.stringify(productos))
+//             }
+//         })
+//     }
