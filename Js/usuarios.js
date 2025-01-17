3442297a464ef3da4845b350862b1d6802ff91b5
Fede1979 <fedesanchez@gmail.com>
2025-01-16 23:53:44 -0300

    agregue opción de e-mail

--- Js/usuarios.js
+++ Js/usuarios.js
@@ -17,9 +17,10 @@
 let registroUsuario = JSON.parse(localStorage.getItem("registroUsuario"));
 
     class DatosUsuario {
-        constructor (nombre, apellido, usuario, contrasena){
+        constructor (nombre, apellido,mail, usuario, contrasena){
             this.nombre = nombre;
             this.apellido = apellido;
+            this.mail = mail;
             this.usuario = usuario;
             this.contrasena = contrasena;
         };
@@ -28,33 +29,26 @@ let registroUsuario = JSON.parse(localStorage.getItem("registroUsuario"));
 let ingresar = document.getElementsByClassName(`botonLogin`)[0];
 
 ingresar.addEventListener("click", function(){
-let usuarioNombre = document.getElementById("nombre").value;
-let usuarioApellido = document.getElementById("apellido").value;
-let usuarioUsuario = document.getElementById("usuario").value;
-let usuarioContrasena = document.getElementById("contrasena").value;
-let usuarioRepetirContrasena = document.getElementById("repetirContrasena").value;
+    let usuarioNombre = document.getElementById("nombre").value;
+    let usuarioApellido = document.getElementById("apellido").value;
+    let usuarioMail = document.getElementById("mail").value;
+    let usuarioUsuario = document.getElementById("usuario").value;
+    let usuarioContrasena = document.getElementById("contrasena").value;
+    let usuarioRepetirContrasena = document.getElementById("repetirContrasena").value;
+                
+    const nuevoUsuario = new DatosUsuario(usuarioNombre, usuarioApellido,usuarioMail, usuarioUsuario, usuarioContrasena);
+    registroUsuario.push(nuevoUsuario);
+    localStorage.setItem("registroUsuario", JSON.stringify(registroUsuario));
 
-    if (usuarioContrasena.length < 8 || usuarioContrasena.length > 12){
-        let contrasenaCorta = document.getElementById("contrasenaIncorrecta");
-        contrasenaCorta.innerHTML = `<p>Debe ingresar entre 8 y 12 números</p>`
-    }else if (usuarioContrasena !== usuarioRepetirContrasena) {
-        contrasenaIncorrecta.innerHTML = `<p>Las contraseñas no coinciden.</p>`;
-    }else{
-        contrasenaIncorrecta.innerHTML = `<p>Acceso permitido.</p>`;
-    };
-            
-const nuevoUsuario = new DatosUsuario(usuarioNombre, usuarioApellido, usuarioUsuario, usuarioContrasena);
-registroUsuario.push(nuevoUsuario);
-localStorage.setItem("registroUsuario", JSON.stringify(registroUsuario));
-
-    if(usuarioNombre === "" || usuarioApellido === "" || usuarioUsuario === "" || usuarioContrasena.length < 8 || usuarioContrasena.length > 12 || usuarioContrasena !== usuarioRepetirContrasena){
-        contrasenaIncorrecta.innerHTML = `<p>Acceso denegado vuelva a intentarlo.</p>`;
-        return;
-    }else{
-        let cambiarBoton = document.getElementById("botonCambiar");
-        cambiarBoton.innerHTML = usuarioNombre;
+        if(usuarioNombre === "" || usuarioApellido === "" || usuarioMail === "" || usuarioUsuario === "" || usuarioContrasena.length < 8 || usuarioContrasena.length > 12 || usuarioContrasena !== usuarioRepetirContrasena){
+            contrasenaIncorrecta.innerHTML = `<p>Acceso denegado vuelva a intentarlo.</p>`;
+            return;
+        }else{
+            let cambiarBoton = document.getElementById("botonCambiar");
+            cambiarBoton.innerHTML = usuarioUsuario;
+        };
     }
-});
+);
 
 
         
