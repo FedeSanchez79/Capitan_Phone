cb9b0e643673978d5c133891466cf05c6fcc4437
Fede1979 <fedesanchez@gmail.com>
2025-01-22 20:45:23 -0300

    elimine main.js y catalogo.js + correcciones a realizar

--- Js/contacto.js
+++
@@ -1,7 +0,0 @@
-//USO DE DOM PARA MODIFICAR EL HTML
-
-let nuevoTitulo = document.getElementById(`info`);
-let tituloContacto = document.createElement(`p`);
-tituloContacto.innerText = `¡Estamos a tu disposición!`;
-nuevoTitulo.appendChild(tituloContacto);
-tituloContacto.className = `estamos`;
--- Js/main.js
+++
@@ -1,4 +0,0 @@
-//USO DE DOM PARA MODIFICAR EL HTML
-
-let tituloPrincipal = document.getElementById(`titulo`);
-tituloPrincipal.innerHTML = `Capitan Phone`;
