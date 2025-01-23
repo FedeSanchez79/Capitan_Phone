4bba9686193495f9d9c1e29679a63e29be934f78
Fede1979 <fedesanchez@gmail.com>
2025-01-23 15:35:31 -0300

    arregle catalogo.js

--- Js/catalogo.js
+++ Js/catalogo.js
@@ -1,96 +1,53 @@
-//CREAR UN FORM USANDO DOM
-
-const items = document.getElementById("items");
-const menuCarga = document.createElement("div");
-menuCarga.innerHTML = ` <form id="menuCargaCatalogo" action="" method="post">
-                        <p>Nuevo Item</p>
-                        <input type="text" id="marca" placeholder="Ingrese la marca">
-                        <input type="text" id="modelo" placeholder="Ingrese el modelo">
-                        <input type="number" id="precio" placeholder="Ingrese el precio">
-                        <button id="agregar" type="button">Agregar</button>
-                        </form>`;
-items.appendChild(menuCarga);
-
-//FUNCIÓN CONSTRUCTORA PARA CREAR LOS OBJETOS
-
-const productos = JSON.parse(localStorage.getItem("productos")) || [];
-
-class datoProducto {
-    static id = productos.length;
-    constructor(marca, modelo, precio) {
-        this.id = ++datoProducto.id;
-        this.marca = marca;
-        this.modelo = modelo;
-        this.precio = precio;
-    };
-};
-
-//ACCESO AL DOM, ALMACENAR EN LOCALSTORAGE Y VERIFICAR QUE LOS DATOS INGRESADOS SEAN CORRECTOS
-
-function agregarProducto() {
-    const marca = document.getElementById("marca").value;
-    const modelo = document.getElementById("modelo").value;
-    const precio = document.getElementById("precio").value;
-    const nuevoProducto = new datoProducto(marca, modelo, precio);
-    productos.push(nuevoProducto);
-    localStorage.setItem("productos", JSON.stringify(productos));
-    localStorage.setItem("productosCarrito", JSON.stringify(productos));
-    tryFunction()
-};
-
-//ASINCRONISMO Y PROMESAS
-
-function tryFunction(){
-    const catalogoCard = document.getElementById("contenedorProductosAdmin");
-    marcaIncorrecta = document.createElement("div");
-    marcaIncorrecta.id = "marcaIncorrecta";
-    let marcaIncorrecta = document.getElementById("marcaIncorrecta");
-    catalogoCard.appendChild(marcaIncorrecta);
-
-    
-    function modeloChecker(modelo){
-        return new Promise(resolve, reject) 
-    }
-        // try{
-        //     if (productos.marca === "xiaomi" || productos.marca === "motorola" || productos.marca === "iphone" || productos.marca === "samsung"){
-        //         marcaIncorrecta.textContent = "Marca ingresada de forma correcta";
-        //     }else{
-        //         throw new Error("Marca ingresada incorrecta. Ingrese: Samsung, Motorola, Xiaomi o iPhone únicamente.")
-        //     }       
-        // }catch(err){
-        //     marcaIncorrecta = err
-        // }finally{
-        //     mostrarProductos();
-        // }   
-}
-
-//ACCESO AL DOM Y MÉTODO FOREACH PARA PODER MOSTRAR CADA PRODUCTO AGREGADO EN LA FUNCIÓN ANTERIOR
-
-function mostrarProductos() {
-    const catalogoCard = document.getElementById("contenedorProductosAdmin");
-    catalogoCard.innerHTML = "";
-    productos.forEach(producto => {
-        const card = document.createElement("div"); 
-        card.className = "contenedor"
-        card.innerHTML = `<img class="productoImagen" src="../assets/images/iphone_imagen.jpg" alt="imagen de un iphone">
-                          <div class="productoInfo">
-                          <h3 class="modelo">${itemNuevo.marca}</h3>
-                          <p class="modelo">${itemNuevo.modelo}</p>
-                          <p class="precio">${itemNuevo.precio}</p>
-                          <button class="agregar">Agregar</button>`;
-        listado.appendChild(card)                  
-    })
-    agregarEventoBotones()
+// REHACER EL CODIGO DE CATALOGO
+const productos = [
+    {
+        id: 0,
+        marca: "Iphone",
+        modelo: "11",
+        precio: 600
+    },
+    {
+        id: 0,
+        marca: "Motorola",
+        modelo: "G84",
+        precio: 400
+    },
+    {
+        id: 0,
+        marca: "SAmsung",
+        modelo: "A15",
+        precio: 350
+    },
+    {
+        id: 0,
+        marca: "Xiaomi",
+        modelo: "Redmi 13C",
+        precio: 300
+    },
+];
+let catalogoProductos = [];
+let listado = document.getElementById("contenedorProductos");
+function agregarItem (item){
+    item.forEach((itemNuevo) => {
+        const card = document.createElement("div");
+          card.innerHTML = `<img class="productoImagen" src="../assets/images/iphone_imagen.jpg" alt="imagen de un iphone">
+        <div class="productoInfo">
+        <h3 class="modelo">${itemNuevo.marca}</h3>
+        <p class="modelo">${itemNuevo.modelo}</p>
+        <p class="precio">${itemNuevo.precio}</p>
+        <button class="agregar">Agregar</button>`;
+listado.appendChild(card)                  
+})
+agregarEventoBotones()
 }
 agregarItem(productos)
-
 
 let contadorCarrito = 0;
 const numero = document.getElementById("numero1");
 
 function incrementarCarrito() {
-     contadorCarrito++;
-     numero.textContent = contadorCarrito;
+    contadorCarrito++;
+    numero.textContent = contadorCarrito;
 }
 
 function agregarEventoBotones() {
@@ -107,104 +64,3 @@ for (const marca of marcas){
     marcaTelefonos.innerHTML = marca
     listaMarcas.appendChild(marcaTelefonos)                            
 }
-
-
-
-
-
-
-//CREAR UN FORM USANDO DOM
-
-// const items = document.getElementById("items");
-// const menuCarga = document.createElement("div");
-// menuCarga.innerHTML = ` <form id="menuCargaCatalogo" action="" method="post">
-//                         <p>Nuevo Item</p>
-//                         <input type="text" id="marca" placeholder="Ingrese la marca">
-//                         <input type="text" id="modelo" placeholder="Ingrese el modelo">
-//                         <input type="number" id="precio" placeholder="Ingrese el precio">
-//                         <button id="agregar" type="button">Agregar</button>
-//                         </form>`;
-// items.appendChild(menuCarga);
-
-// //FUNCIÓN CONSTRUCTORA PARA CREAR LOS OBJETOS
-
-// const productos = JSON.parse(localStorage.getItem("productos")) || [];
-
-// class datoProducto {
-//     static id = productos.length;
-//     constructor(marca, modelo, precio) {
-//         this.id = ++datoProducto.id;
-//         this.marca = marca;
-//         this.modelo = modelo;
-//         this.precio = precio;
-//     };
-// };
-
-// //ACCESO AL DOM, ALMACENAR EN LOCALSTORAGE Y VERIFICAR QUE LOS DATOS INGRESADOS SEAN CORRECTOS
-
-// function agregarProducto() {
-//     const marca = document.getElementById("marca").value;
-//     const modelo = document.getElementById("modelo").value;
-//     const precio = document.getElementById("precio").value;
-//     const nuevoProducto = new datoProducto(marca, modelo, precio);
-//     productos.push(nuevoProducto);
-//     localStorage.setItem("productos", JSON.stringify(productos));
-//     localStorage.setItem("productosCarrito", JSON.stringify(productos));
-// };
-
-// //ACCESO AL DOM Y MÉTODO FOREACH PARA PODER MOSTRAR CADA PRODUCTO AGREGADO EN LA FUNCIÓN ANTERIOR
-
-// function mostrarProductos() {
-//     const catalogoCard = document.getElementById("contenedorProductosAdmin");
-//     catalogoCard.innerHTML = "";
-//     productos.forEach(producto => {
-//         const card = document.createElement("div"); 
-//         card.className = "contenedor"
-//         card.innerHTML = `<img class="productoImagen" src="../assets/images/iphone_imagen.jpg" alt="imagen de un iphone">
-//                           <h3 class="modelo">${producto.marca}</h3>
-//                           <p class="modelo">${producto.modelo}</p>
-//                           <p class="precio">u$s ${producto.precio}</p>
-//                           <button class="agregar">Comprar</button>
-//                           <button class="botonEliminar">Eliminar Item</button>`;                 
-//         catalogoCard.appendChild(card);
-//         const botonEliminar = card.querySelector(".botonEliminar");
-//         botonEliminar.onclick = () => eliminarDelCatalogo(producto.id);
-//     });
-//     agregarEventoBotones();
-// };
-
-
-// //FUNCIÓN PARA ELIMINAR LOS OBJETOS CREADOS PREVIAMENTE USANDO EL EVENTO ONLICK DENTRO DE LA FUNCIÓN mostrarProductos()
-
-// function eliminarDelCatalogo(id) {
-//     const nuevoCatalogo = productos.filter(producto => producto.id !== id);
-//     productos.length = 0;
-//     nuevoCatalogo.forEach(producto => productos.push(producto));
-//     localStorage.setItem("productos", JSON.stringify(productos));
-//     mostrarProductos();
-// };
-
-// //EVENTO ONCLICK PARA AGREGAR PRODUCTO
-
-// const botonAgregar = document.getElementById("agregar");
-// botonAgregar.onclick = (e) => {agregarProducto();};
-
-
-// function carritoAgregar(marca, precio){
-// const carritoMarca = productos.filter(producto => producto.marca !== marca)
-// productos.length = 0;
-// carritoMarca.forEach(producto => productos.push(producto))
-// const carritoPrecio = productos.filter(producto => producto.precio !== precio)
-// productos.length = 0;
-// carritoPrecio.forEach(producto => productos.push(producto))
-// localStorage.setItem("productosCarrito", JSON.stringify(productos));
-// mostrarProductos();
-// }
-// //EJECUTO LA FUNCIÓN PARA MOSTRAR LOS PRODUCTOS
-
-// mostrarProductos();
-
-// //FUNCIÓN Y MÉTODOS PARA PODER USAR EL SELECT Y FILTRAR POR OPCIÓN ELEGIDA (**PENDIENTE**)
-
-// let seleccion = JSON.parse(localStorage.getItem("productos"))
-// let seleccionMarca = document.getElementById("select1")
