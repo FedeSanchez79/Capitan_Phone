c7edff72ebb3334d813984e5942ef70420cc0652
Fede1979 <fedesanchez@gmail.com>
2025-01-25 17:23:05 -0300

    boton finalizar compra

--- Js/carrito.js
+++ Js/carrito.js
@@ -1,14 +1,17 @@
-let carrito = document.getElementById("items")
+let carrito = document.getElementById("items");
+let mensajeCarrito = document.getElementById("mensajeCarrito");
+let menuTotal = document.getElementById("menuTotal");
 
 let itemsCatalogo = JSON.parse(localStorage.getItem("carrito")) || [];
 
 function mostrarCarrito(carritoItems) {
     if (!carritoItems || carritoItems.length === 0) {
-        return actualizarVistaCarrito();;
+        return actualizarVistaCarrito();
     }
+
     carritoItems.forEach((listadoItems) => {
         const items = document.createElement("div");
-        items.className = "itemCarrito"
+        items.className = "itemCarrito";
         items.innerHTML = `<div><small>Celular</small>
                            <h3>${listadoItems.marca}</h3></div>
                            <div><small id="tituloCantidad">Cantidad</small></br> 
@@ -18,51 +21,48 @@ function mostrarCarrito(carritoItems) {
                            <div><small>Subtotal</small>
                            <p id="subtotal">u$s ${listadoItems.cantidad * listadoItems.precio} </p></div>
                            <div><i id="deleteCarrito" class="bi bi-trash"></i></div>`;
-        carrito.appendChild(items);
-
-        //TOTAL DE CARRITO
-
-        const totalCarrito = document.getElementById("items");
-        const inputTotal = document.createElement("div");
-        inputTotal.innerHTML = `<h4>Total:</h4>
-                                <input id="total" placeholder="Total"></input>`
-        totalCarrito.appendChild(inputTotal)  
-
-        function actualizarTotal() {
-        let total = 0;
-        total = itemsCatalogo.reduce((acumulador, producto) => {
-            return acumulador + (producto.cantidad * producto.precio);
-            }, 0);
-        const elementoTotal = document.getElementById("total");
-            if (elementoTotal) {
-            elementoTotal.value = `u$s ${total.toFixed(2)}`;
-            }
-        }
-        actualizarTotal();
+        mensajeCarrito.appendChild(items);
     });
-    const limpiarCarrito = document.getElementById("items");
-        if (limpiarCarrito) {
-            const botonVaciar = document.createElement("div");
-            botonVaciar.innerHTML = `<button id="vaciar">Vaciar carrito</button>`;
-            limpiarCarrito.appendChild(botonVaciar);
-            const vaciar = document.getElementById("vaciar");
-            vaciar.addEventListener("click", limpiarLocal);
-        }
-}
-mostrarCarrito(itemsCatalogo)
 
+    let inputTotal = document.createElement("div");
+    inputTotal.innerHTML = `<h4>Total:</h4>
+                            <input id="total" placeholder="Total" readonly></input>
+                            <button id="finalizarCompra"><a href="./construccion.html">Finalizar Compra</button>`;
+    menuTotal.appendChild(inputTotal);
+
+    actualizarTotal();
+
+    const botonVaciar = document.createElement("div");
+    botonVaciar.innerHTML = `<button id="vaciar">Vaciar carrito</button>`;
+    menuTotal.appendChild(botonVaciar);
+
+    const vaciar = document.getElementById("vaciar");
+    vaciar.addEventListener("click", limpiarLocal);
+}
+
+function actualizarTotal() {
+    let total = 0;
+    total = itemsCatalogo.reduce((acumulador, producto) => {
+        return acumulador + (producto.cantidad * producto.precio);
+    }, 0);
+    
+    const elementoTotal = document.getElementById("total");
+    if (elementoTotal) {
+        elementoTotal.value = `u$s ${total.toFixed(2)}`;
+    }
+}
 
 function limpiarLocal() {
     localStorage.removeItem("carrito");
     itemsCatalogo.length = 0;
     actualizarVistaCarrito();
     actualizarTotal();
-} 
+}
 
 function actualizarVistaCarrito() {
-    const carritoListado = document.getElementById("items"); 
-    if (carritoListado) {
-        carritoListado.innerHTML = ""; 
-        carritoListado.innerHTML = `<p class="carritoVacio">Tu carrito está vacío <i class="bi bi-emoji-frown"></i></p>`;
-    } 
+    mensajeCarrito.innerHTML = "";
+    menuTotal.innerHTML = "";
+    mensajeCarrito.innerHTML = `<p class="carritoVacio">Tu carrito está vacío <i class="bi bi-emoji-frown"></i></p>`;
 }
+
+mostrarCarrito(itemsCatalogo);
