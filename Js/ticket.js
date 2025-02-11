20c7c15785078b43caf3f1a1f5540c1e292aca0c
Fede1979 <fedesanchez@gmail.com>
2025-02-10 23:34:45 -0300

    estilos de ticket

--- Js/ticket.js
+++ Js/ticket.js
@@ -1,12 +1,14 @@
-// Recuperamos los datos de la compra desde localStorage
+// 1. RECUPERO LOS DATOS DE USUARIO Y DEL CARRITO
+// 2. FUNCION PARA LIMPIAR EL LOCALSTORAGE LUEGO DE LA COMPRA
+
+// ***1***
+
 const datosCompra = JSON.parse(localStorage.getItem("datosCompra"));
 
 if (datosCompra) {
-    // Mostrar los datos del usuario
     document.getElementById("nombreUsuario").textContent = `Nombre: ${datosCompra.usuario.nombre} ${datosCompra.usuario.apellido}`;
     document.getElementById("emailUsuario").textContent = `Correo electrónico: ${datosCompra.usuario.mail}`;
 
-    // Mostrar los productos del carrito
     const listaProductos = document.getElementById("listaProductos");
     datosCompra.productos.forEach(producto => {
         const item = document.createElement("li");
@@ -14,26 +16,19 @@ if (datosCompra) {
         listaProductos.appendChild(item);
     });
 
-    // Mostrar el total
-    document.getElementById("totalAmount").textContent = `u$s ${datosCompra.total.toFixed(2)}`;
-} else {
-    // Si no hay datos de la compra, redirigir a la página principal
-    window.location.href = "../index.html"; // O la ruta de tu página principal
-}
+    document.getElementById("cantidadTotal").textContent = `u$s ${datosCompra.total.toFixed(2)}`;
+} 
 
-// Agrega el evento para el botón "Volver al inicio"
-document.getElementById("volverInicio").addEventListener("click", function(event) {
-    // Evitar que el enlace funcione de inmediato
-    event.preventDefault();
+// ***2***
+
+document.getElementById("volverInicio").addEventListener("click", function() {
 
         limpiarLocal();
-        // Redirigir al inicio
         window.location.href = "../index.html"; 
+
 });
 
-// Función para limpiar el carrito
 function limpiarLocal() {
     localStorage.removeItem("carrito");
     localStorage.removeItem("datosCompra");
-    // Aquí puedes agregar más elementos a eliminar si es necesario
 }
